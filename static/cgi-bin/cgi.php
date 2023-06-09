27a5bc5e22255bb47c9e54771a22dfd512e01465
Hamza Limouri <limouri.hamza@gmail.com>
2023-06-09 13:52:28 +0100

    Added static files.

---
+++ static/cgi-bin/cgi.php
@@ -0,0 +1,30 @@
+#!/usr/bin/php
+<?php
+// Send the HTTP headers
+echo "Status: 200 OK\n";
+echo "Content-type: text/html\r\n\r\n";
+
+
+// Generate the HTML response
+echo "<!DOCTYPE html>\n";
+echo "<html>\n";
+echo "<head>\n";
+echo "<title>PHP CGI Script</title>\n";
+echo "</head>\n";
+echo "<body>\n";
+echo "<h1>Hello, this is a PHP CGI script!</h1>\n";
+
+// Display some server and request information
+echo "<h2>Server Information</h2>\n";
+echo "<pre>\n";
+print_r($_SERVER);
+echo "</pre>\n";
+
+echo "<h2>Request Information</h2>\n";
+echo "<pre>\n";
+print_r($_REQUEST);
+echo "</pre>\n";
+
+echo "</body>\n";
+echo "</html>\n";
+?>
